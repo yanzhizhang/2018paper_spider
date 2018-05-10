a3d903cf37a60dec3d48b5ae3c8f660887f10fdf
leoyanzhizhang <fantasyleonzhang@hotmail.com>
2018-05-10 16:39:57 -0400

    done title author and href

--- iclr/iclr/spiders/iclr_body.py
+++ iclr/iclr/spiders/iclr_body.py
@@ -5,6 +5,7 @@ import csv
 
 class PapersFile(scrapy.Item):
     author = scrapy.Field()
+    title = scrapy.Field()
     href = scrapy.Field()
 
 
@@ -23,6 +24,20 @@ class CvprSpider(scrapy.Spider):
         page = response.url.split("/")[-2]
 
         filename = 'body-%s.html' % page
+
+        authors_list = response.css("div.maincardFooter").re(r">(.*)<")
+        titles_list = response.css("div.maincardBody").re(r'Body">(.*)</div>')
+        hrefs_list = response.selector.xpath('//a[contains(@href,"open")]').re('(https://openreview.net/forum\?id=.*)" class="btn b')
+
+
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)
+
+
+        with open('output.csv', 'w', newline='') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=['authors', 'titles', 'hrefs'])
+            writer.writeheader()
+
+            for i in range(len(authors_list)):
+                writer.writerow({"authors": authors_list[i],"titles": titles_list[i],"hrefs": hrefs_list[i]})
