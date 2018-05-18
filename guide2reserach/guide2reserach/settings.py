ca4342b126d7399592129cf96e7060dcaa2e2e3c
leoyanzhizhang <fantasyleonzhang@hotmail.com>
2018-05-18 13:44:34 -0400

    guide2reserach done

--- guide2reserach/guide2reserach/settings.py
+++ guide2reserach/guide2reserach/settings.py
@@ -39,10 +39,10 @@ ROBOTSTXT_OBEY = True
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
-#}
+DEFAULT_REQUEST_HEADERS = {
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+  'Accept-Language': 'en-US',
+}
 
 # Enable or disable spider middlewares
 # See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
--- guide2reserach/guide2reserach/spiders/top1000research.py
+++ guide2reserach/guide2reserach/spiders/top1000research.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import xlsxwriter
+
+global_authors_list = []
+global_affliation_list = []
 
 
 class Top1000researchSpider(scrapy.Spider):
@@ -8,21 +12,20 @@ class Top1000researchSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'http://www.guide2research.com/scientists/page-1'
-            'http://www.guide2research.com/scientists/page-2'
-            'http://www.guide2research.com/scientists/page-3'
-            'http://www.guide2research.com/scientists/page-4'
-            'http://www.guide2research.com/scientists/page-5'
-            'http://www.guide2research.com/scientists/page-6'
-            'http://www.guide2research.com/scientists/page-7'
-            'http://www.guide2research.com/scientists/page-8'
-            'http://www.guide2research.com/scientists/page-9'
+            'http://www.guide2research.com/scientists/page-1',
+            'http://www.guide2research.com/scientists/page-2',
+            'http://www.guide2research.com/scientists/page-3',
+            'http://www.guide2research.com/scientists/page-4',
+            'http://www.guide2research.com/scientists/page-5',
+            'http://www.guide2research.com/scientists/page-6',
+            'http://www.guide2research.com/scientists/page-7',
+            'http://www.guide2research.com/scientists/page-8',
+            'http://www.guide2research.com/scientists/page-9',
             'http://www.guide2research.com/scientists/page-10'
         ]
 
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
-
 
     def parse(self, response):
         page = response.url.split("/")[-2]
@@ -30,15 +33,25 @@ class Top1000researchSpider(scrapy.Spider):
         filename = 'body-%s.html' % page
 
         authors_list = response.xpath('//table')[3:103].re(r'<h3 style="margin-bottom:1px;"><a href=".*>(.*)</a></h3>\n\t\t\t\t')
-        affliation_list = response.xpath('//table')[3:102].re(r'"http://www.guide2research.com/scientists/uni-[0-9]*">([a-zA-Z| ]*)')
-
-        temp_list = []
-        for i in range(len(authors_list)):
-            print(authors_list[i])
-            print(affliation_list[i])
-
-            temp_list.append( [authors_list[i], affliation_list[i]] )
+        affliation_list = response.xpath('//table')[3:103].re(r'"http://www.guide2research.com/scientists/uni-[0-9]*">([a-zA-Z| ]*)')
 
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)
+
+        global_authors_list.extend(authors_list)
+        global_affliation_list.extend(affliation_list)
+
+        workbook = xlsxwriter.Workbook('list.xlsx')
+        worksheet = workbook.add_worksheet()
+
+        bold = workbook.add_format({'bold': 1})
+        worksheet.write('A1', 'Name', bold)
+        worksheet.write('B1', 'Affliation', bold)
+
+        row = 1
+        for i in range(len(global_authors_list)):
+            worksheet.write_string  (row, 0, global_authors_list[i])
+            worksheet.write_string  (row, 1, global_affliation_list[i])
+            row += 1
+        workbook.close()
